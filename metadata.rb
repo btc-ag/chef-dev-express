f4446727c30747a167eaf809d5159a78b9daa208
cldieder.btc <claas.diederichs@btc-ag.com>
2019-01-25 12:31:00 +0100

    bup version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bop-infra@btc-ag.com'
 license 'MIT'
 description 'Installs/removes dev_express'
 long_description 'Installs/Configures dev_express'
-version '1.0.2'
+version '1.0.3'
 chef_version '>= 13.0'
 supports 'windows'
 
